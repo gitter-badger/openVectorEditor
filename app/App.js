b7bf3f54bf0eacbd1343a430ccc85b9aeb51f6a2
Allison Fero <afero@PleasurePoint.local>
2015-06-26 14:19:09 -0700

    making a dnaLibrary in React

--- app/App.js
+++ app/App.js
@@ -1,88 +1,125 @@
 var React = require('react');
-var Router = require('react-router'); // or var Router = ReactRouter; in browsers
-var Route = Router.Route;
-var RouteHandler = Router.RouteHandler;
-var Link = Router.Link;
+var data = {
+  bins:[{
+    numberOfRows: 3
+  },{
+    numberOfRows: 2
+  },{
+    numberOfRows: 2
+  }]
+}
 
-var Dashboard = require('./Dashboard.js');
-var SequenceLibrary = require('./SequenceLibrary.js');
-var SequenceEditor2 = require('./SequenceEditor2.js');
-var SequenceEditor3 = require('./SequenceEditor3.js');
-var Skeleton = require('./Skeleton.js');
+// var WellRow = React.createClass {
+//   render: function() {
+//     var well = this.props.fragments;
+//   }
+// }
 
-var Login = require('./Login.js');
-var Logout = require('./Logout.js');
-var Authentication = require('./Authentication.js');
-var About = require('./About.js');
-var auth = require('./auth.js');
-
-var baobabMixin = require('baobab-react/mixins').root;
-var baobabTree = require('./baobabTree');
 
 var App = React.createClass({
-
-  mixins: [baobabMixin],
-
-  contextTypes: {
-    router: React.PropTypes.func.isRequired
-  },
-
-  getInitialState: function () {
-    return {
-      loggedIn: auth.loggedIn()
-    };
-  },
-
-  setStateOnAuth: function (loggedIn) {
-    this.setState({
-      loggedIn: loggedIn
-    });
-  },
-
-  componentWillMount: function () {
-    auth.onChange = this.setStateOnAuth;
-    auth.login();
-  },
-
   render: function () {
-    var loginOrOut = this.state.loggedIn ?
-      <Link to="logout">Log out</Link> :
-      <Link to="login">Sign in</Link>;
+
+    var rowIndexArray = [];
+    var binArray = [];
+    function makeCombinations (choices, callback, prefix) {
+        if(!choices.length) {
+            return callback(prefix);
+        }
+        for(var c = 0; c < choices[0].length; c++) {
+            makeCombinations(choices.slice(1), callback, (prefix || []).concat(choices[0][c]));
+        }
+    }
+    function makeRowIndexArray (indices) {
+      if(typeof console == "object"){
+          rowIndexArray.push(indices);
+      }
+    }
+
+    var fragments = 0;
+    var totalCombinations = 1;
+    var combinations = 1;
+    this.props.data.bins.forEach(function(bin) {
+      fragments = bin.numberOfRows + fragments;
+      combinations = combinations * bin.numberOfRows;
+      var styleSwitchIndex = totalCombinations/combinations;
+      var rowIndex = 0;
+      var binRows = []
+      for (var col=0; col<bin.numberOfRows; col++) {
+        binRows.push(fragments-col);
+      }
+      binArray.push(binRows);
+    });
+    
+    makeCombinations(binArray, makeRowIndexArray);
+    debugger
+
+    var headers = [];
+    var rows = [];
+    for (var i = 0; i < fragments; i++) {
+      headers.push(i+1);
+    }
+    for (var i = 0; i < combinations; i++) {
+      rows.push(i+1);
+    }
+
+    var rowText = 'O';
+
     return (
-      <div>
-        <SequenceEditor2/>
-        <header>
-          <ul>
-            <li>{loginOrOut}</li>
-            <li><Link to="about">About</Link></li>
-            <li><Link to="dashboard">Dashboard</Link></li>
-          </ul>
-        </header>
-        <RouteHandler/>
+      <div className ="wellTable">
+        <table style={{width:'100%'}}>
+        {headers.map(function(header) {
+          return (<td>
+            <th> {"Frag_" + header} </th>
+            </td>)
+        })}
+        {rows.map(function(row) {
+          return (
+            <tr>
+            <th colSpan="1"> {"Combo_" + row} </th>
+            </tr>
+            )
+        })}
+        {rows.map(function(row) {
+            headers.map(function(header) {
+            return (
+            <td dataAlign = "right"> test </td>
+            )
+          })
+        })}
+        </table>
       </div>
-    );
+      )
+
+                // {headers.map(function (header) {
+            //   return (<td> test </td>)
+            //   })}
+    // return (
+    //   <div>
+    //       {
+    //         var bins = data.bins
+    //       }
+    //       // {data.bins.map(function (rows) {
+    //       //   var binNumber = 5 + 1;  
+    //       //   return (<th>
+    //       //     <td>{rows}</td>
+    //       //   </th>)
+    //       // })}
+    //   <table style={{width:'100%'}}>
+    //       <tr>
+    //         <td> hi </td>
+    //       </tr>
+    //       <tr>
+    //         <td>{bins}</td>
+    //         <td>Jackson</td> 
+    //         <td>94</td>
+    //       </tr>
+    //     </table>
+    //   </div>
+    // );
   }
 });
-            // <li><Link to="about">About</Link></li>
-            // <li><Link to="dashboard">Dashboard</Link></li>
-            // <li><Link to="sequences">Sequences</Link></li>
-            // <li><Link to="SequenceEditor2">SequenceEditor5</Link></li>
 
-var routes = (
-  <Route handler={App}>
-    <Route path="/login" name="login" handler={Login}/>
-    <Route path="/logout" name="logout" handler={Logout}/>
-    <Route path="/about" name="about" handler={About}/>
-
-    <Route path="" name="Skeleton" handler={Skeleton}>
-      <Route path="/sequenceLibrary" name="sequences" handler={SequenceLibrary}/>
-      <Route path="/dashboard" name="dashboard" handler={Dashboard}/>
-      <Route path="/sequenceEditor2" name="SequenceEditor2" handler={SequenceEditor2}/>
-      <Route path="/sequenceEditor3" name="SequenceEditor3" handler={SequenceEditor3}/>
-    </Route>
-  </Route>
+React.render(
+  <App data={data}/>,
+  document.body
 );
-
-Router.run(routes, function (Handler) {
-  React.render(<Handler tree={baobabTree}/>, document.body);
-});
--- app/getXCenterOfRowAnnotation.js
+++
@@ -1,5 +0,0 @@
-module.exports = function getXCenterOfRowAnnotation(range, bpsPerRow, charWidth) {
-	var getXStartAndWidthOfRowAnnotation = require('./getXStartAndWidthOfRowAnnotation');
-	var {xStart, width} = getXStartAndWidthOfRowAnnotation(range, bpsPerRow, charWidth);
-	return xStart + width/2;
-};
